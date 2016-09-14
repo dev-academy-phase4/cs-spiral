28e0b89dd35773624e351d06c7c454a1c88ae8d1
Rich Churcher <rich.churcher@gmail.com>
2016-09-15 10:04:51 +1200

    First tests

---
+++ Spiral/Board.cs
@@ -0,0 +1,21 @@
+﻿namespace Spiral
+{
+    public class Board
+    {
+        public char[][] Cells { get; set; }
+
+        public Board(int rows, int cols)
+        {
+            Cells = new char[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                Cells[i] = new char[cols];
+                for (int j = 0; j < cols; j++)
+                {
+                    Cells[i][j] = '.';
+                }
+            }
+        }
+    }
+}
---
+++ SpiralTests/BoardTests.cs
@@ -0,0 +1,19 @@
+﻿using Spiral;
+using Xunit;
+
+namespace SpiralTests
+{
+    public class BoardTests
+    {
+        [Fact]
+        public void BoardGeneratedWithCorrectDimensions ()
+        {
+            // Arrange & Act
+            var board = new Board(5, 5);
+
+            // Assert
+            Assert.Equal(5, board.Cells.Length);
+            Assert.Equal(5, board.Cells[0].Length);
+        }
+    }
+}

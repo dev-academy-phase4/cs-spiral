6595921bc32618c26a0be8b1904dec82abdfbfbe
Rich Churcher <rich.churcher@gmail.com>
2016-09-17 16:17:47 +1200

    Player location & movement

---
+++ Spiral/Player.cs
@@ -0,0 +1,56 @@
+﻿namespace Spiral
+{
+    public class Player
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public Player ()
+        {
+            Row = 0;
+            Col = 0;
+        }
+
+        public void Move(string direction)
+        {
+            switch (direction)
+            {
+                case "s":
+                    Row++;
+                    break;
+
+                case "e":
+                    Col++;
+                    break;
+
+                case "n":
+                    Row--;
+                    break;
+
+                case "w":
+                    Col--;
+                    break;
+
+                case "se":
+                    Row++;
+                    Col++;
+                    break;
+
+                case "sw":
+                    Row++;
+                    Col--;
+                    break;
+
+                case "ne":
+                    Row--;
+                    Col++;
+                    break;
+
+                case "nw":
+                    Row--;
+                    Col--;
+                    break;
+            }
+        }
+    }
+}
---
+++ SpiralTests/PlayerTests.cs
@@ -0,0 +1,137 @@
+﻿using Spiral;
+using Xunit;
+
+namespace SpiralTests
+{
+    public class PlayerTests
+    {
+        [Fact]
+        public void PlayerStartsAt0 ()
+        {
+            // Arrange & Act
+            var player = new Player();
+
+            // Assert
+            Assert.Equal(0, player.Row);
+            Assert.Equal(0, player.Col);
+        }
+
+        [Fact]
+        public void MoveSouth ()
+        {
+            // Arrange
+            var player = new Player();
+
+            // Act
+            player.Move("s");
+
+            // Assert
+            Assert.Equal(1, player.Row);
+            Assert.Equal(0, player.Col);
+        }
+
+        [Fact]
+        public void MoveEast ()
+        {
+            // Arrange
+            var player = new Player();
+
+            // Act
+            player.Move("e");
+
+            // Assert
+            Assert.Equal(0, player.Row);
+            Assert.Equal(1, player.Col);
+        }
+
+        [Fact]
+        public void MoveNorth ()
+        {
+            // Arrange
+            var player = new Player();
+            player.Row = 1;
+
+            // Act
+            player.Move("n");
+
+            // Assert
+            Assert.Equal(0, player.Row);
+            Assert.Equal(0, player.Col);
+        }
+
+        [Fact]
+        public void MoveWest ()
+        {
+            // Arrange
+            var player = new Player();
+            player.Col = 1;
+
+            // Act
+            player.Move("w");
+
+            // Assert
+            Assert.Equal(0, player.Row);
+            Assert.Equal(0, player.Col);
+        }
+
+        [Fact]
+        public void MoveSouthEast ()
+        {
+            // Arrange
+            var player = new Player();
+
+            // Act
+            player.Move("se");
+
+            // Assert
+            Assert.Equal(1, player.Row);
+            Assert.Equal(1, player.Col);
+        }
+
+        [Fact]
+        public void MoveSouthWest ()
+        {
+            // Arrange
+            var player = new Player();
+            player.Col = 1;
+
+            // Act
+            player.Move("sw");
+
+            // Assert
+            Assert.Equal(1, player.Row);
+            Assert.Equal(0, player.Col);
+        }
+
+        [Fact]
+        public void MoveNorthEast ()
+        {
+            // Arrange
+            var player = new Player();
+            player.Row = 1;
+
+            // Act
+            player.Move("ne");
+
+            // Assert
+            Assert.Equal(0, player.Row);
+            Assert.Equal(1, player.Col);
+        }
+
+        [Fact]
+        public void MoveNorthWest ()
+        {
+            // Arrange
+            var player = new Player();
+            player.Row = 1;
+            player.Col = 1;
+
+            // Act
+            player.Move("nw");
+
+            // Assert
+            Assert.Equal(0, player.Row);
+            Assert.Equal(0, player.Col);
+        }
+    }
+}
